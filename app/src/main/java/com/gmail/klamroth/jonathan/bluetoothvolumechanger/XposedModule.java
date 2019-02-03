9792f1a971523f42d7c031fd604ca9c8c38f2f78
Jonathan Klamroth <jonathan.klamroth@gmail.com>
2019-02-03 19:07:25 +0100

    add xposed framework

---
+++ app/src/main/java/com/gmail/klamroth/jonathan/bluetoothvolumechanger/XposedModule.java
@@ -0,0 +1,15 @@
+package com.gmail.klamroth.jonathan.bluetoothvolumechanger;
+
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+
+public class XposedModule implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage (XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    }
+
+}
